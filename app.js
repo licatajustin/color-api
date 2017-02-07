c57077e72b6276338baf3bb5db4778f10bb43ac2
Justin Licata <licata.justin@gmail.com>
2017-02-06 19:45:42 -0600

    add Google Analytics and Tweet button.

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var express = require('express')
 var favicon = require('serve-favicon')
 var app = express()
 var path = require('path')
-var Utils = require('./utils.js')
+var Utils = require('./utils')
 var tinycolor = require('tinycolor2')
+var expressGoogleAnalytics = require('express-google-analytics')
+var analytics = expressGoogleAnalytics('UA-91566104-1')
 
 app.use(favicon(__dirname + '/public/favicon.ico'))
 app.set('json spaces', 2)
+app.use(analytics)
 
 app.get('/', function(_req, res) {
   res.sendFile(path.join(__dirname + '/index.html'))
